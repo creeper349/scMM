1619a2bf7dc965bff0a59dba78a62f8063f33841
boyazhao51 <73950634+creeper349@users.noreply.github.com>
2026-09-21 14:36:06 +0800

    Delete run.py

--- run.py
+++
@@ -1,9 +0,0 @@
-from scMM.file.data import CyESIData
-import logging
-import numpy as np
-logging.basicConfig(level=logging.INFO)
-
-#data = CyESIData.load_from_filelist("/home/zby/scMM/data/3d-models/test", ref_mz = 734.5929, prominence_ratio = 0.01)
-data = CyESIData.load_from_filelist("/home/zby/scMM/data/3d-models/20260329-yz-0mM", ref_mz = 734.5929,
-                    ppm_tol = 10, cell_snr = 5.0, peak_snr = 1.0, ms_peak_snr_threshold = 10.0, n_jobs=-1, dtype=np.float32)
-data.save("/home/zby/scMM/data/algea_results/raw/0mM-1")
